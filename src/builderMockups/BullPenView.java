3dabb3d4dfeaca53a508d77b8766b8c791031134
dfontana <dmz.sfx@gmail.com>
2016-04-02 16:42:21 -0400

    Changed Structure

--- src/builderMockups/BullPenView.java
+++ src/builderMockups/BullPenView.java
@@ -6,148 +6,44 @@ import javax.swing.GroupLayout;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.ScrollPaneConstants;
-import javax.swing.GroupLayout.Alignment;
 
 public class BullpenView extends JScrollPane {
 	GroupLayout gl_panelScrollContainer;
 	JPanel panelScrollContainer;
-	PieceGroupView piece1, piece2, piece3, piece4, piece5, piece6, piece7, piece8,
-		piece9, piece10, piece11, piece12, piece13, piece14, piece15, piece16, 
-		piece17, piece18, piece19, piece20, piece21, piece22, piece23, piece24, piece25,
-		piece26, piece27, piece28, piece29, piece30, piece31, piece32, piece33, piece34,
-		piece35;
-	//#EPIC
-	public BullpenView(){
+	
+	AbstractPieceGroupView availablePieces[];
+	
+	public BullpenView(AbstractPieceGroupView pieces[]){
 		getVerticalScrollBar().setUnitIncrement(35);
 		setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 		setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-		
 		panelScrollContainer = new JPanel();
 		panelScrollContainer.setBackground(Color.WHITE);
 		setViewportView(panelScrollContainer);
 		
-		gl_panelScrollContainer = new GroupLayout(panelScrollContainer);
-		
-		piece1 = new PieceGroupView(1);
-		piece2 = new PieceGroupView(2);
-		piece3 = new PieceGroupView(3);
-		piece4 = new PieceGroupView(4);
-		piece5 = new PieceGroupView(5);
-		piece6 = new PieceGroupView(6);
-		piece7 = new PieceGroupView(7);
-		piece8 = new PieceGroupView(8);
-		piece9 = new PieceGroupView(9);
-		piece10 = new PieceGroupView(10);
-		piece11 = new PieceGroupView(11);
-		piece12 = new PieceGroupView(12);
-		piece13 = new PieceGroupView(13);
-		piece14 = new PieceGroupView(14);
-		piece15 = new PieceGroupView(15);
-		piece16 = new PieceGroupView(16);
-		piece17 = new PieceGroupView(17);
-		piece18 = new PieceGroupView(18);
-		piece19 = new PieceGroupView(19);
-		piece20 = new PieceGroupView(20);
-		piece21 = new PieceGroupView(21);
-		piece22 = new PieceGroupView(22);
-		piece23 = new PieceGroupView(23);
-		piece24 = new PieceGroupView(24);
-		piece25 = new PieceGroupView(25);
-		piece26 = new PieceGroupView(26);
-		piece27 = new PieceGroupView(27);
-		piece28 = new PieceGroupView(28);
-		piece29 = new PieceGroupView(29);
-		piece30 = new PieceGroupView(30);
-		piece31 = new PieceGroupView(31);
-		piece32 = new PieceGroupView(32);
-		piece33 = new PieceGroupView(33);
-		piece34 = new PieceGroupView(34);
-		piece35 = new PieceGroupView(35);
-		
+		availablePieces = pieces;
 		setupLayout();
 	}
 	
 	private void setupLayout(){
-		gl_panelScrollContainer.setHorizontalGroup(
-				gl_panelScrollContainer.createParallelGroup(Alignment.TRAILING)
-					.addGroup(gl_panelScrollContainer.createSequentialGroup()
-						.addGroup(gl_panelScrollContainer.createParallelGroup(Alignment.LEADING, false)
-							.addComponent(piece1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addComponent(piece2, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addComponent(piece3,GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addComponent(piece4,GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addComponent(piece5,GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addComponent(piece6,GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addComponent(piece7,GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addComponent(piece8,GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addComponent(piece9,GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addComponent(piece10,GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addComponent(piece11,GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addComponent(piece12,GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addComponent(piece13,GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addComponent(piece14,GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addComponent(piece15,GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addComponent(piece16,GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addComponent(piece17,GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addComponent(piece18,GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addComponent(piece19,GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addComponent(piece20,GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addComponent(piece21,GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addComponent(piece22,GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addComponent(piece23,GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addComponent(piece24,GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addComponent(piece25,GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addComponent(piece26,GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addComponent(piece27,GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addComponent(piece28,GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addComponent(piece29,GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addComponent(piece30,GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addComponent(piece31,GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addComponent(piece32,GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addComponent(piece33,GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addComponent(piece34,GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addComponent(piece35,GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-			);
-			gl_panelScrollContainer.setVerticalGroup(
-				gl_panelScrollContainer.createParallelGroup(Alignment.LEADING)
-					.addGroup(gl_panelScrollContainer.createSequentialGroup()
-						.addComponent(piece1, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
-						.addComponent(piece2, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
-						.addComponent(piece3, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
-						.addComponent(piece4, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
-						.addComponent(piece5, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
-						.addComponent(piece6, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
-						.addComponent(piece7, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
-						.addComponent(piece8, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
-						.addComponent(piece9, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
-						.addComponent(piece10, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
-						.addComponent(piece11, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
-						.addComponent(piece12, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
-						.addComponent(piece13, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
-						.addComponent(piece14, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
-						.addComponent(piece15, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
-						.addComponent(piece16, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
-						.addComponent(piece17, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
-						.addComponent(piece18, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
-						.addComponent(piece19, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
-						.addComponent(piece20, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
-						.addComponent(piece21, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
-						.addComponent(piece22, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
-						.addComponent(piece23, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
-						.addComponent(piece24, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
-						.addComponent(piece25, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
-						.addComponent(piece26, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
-						.addComponent(piece27, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
-						.addComponent(piece28, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
-						.addComponent(piece29, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
-						.addComponent(piece30, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
-						.addComponent(piece31, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
-						.addComponent(piece32, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
-						.addComponent(piece33, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
-						.addComponent(piece34, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
-						.addComponent(piece35, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE))
-			);
-			panelScrollContainer.setLayout(gl_panelScrollContainer);
+		gl_panelScrollContainer = new GroupLayout(panelScrollContainer);
+		GroupLayout.ParallelGroup hGroup = gl_panelScrollContainer.createParallelGroup(GroupLayout.Alignment.TRAILING);
+		GroupLayout.ParallelGroup vGroup = gl_panelScrollContainer.createParallelGroup(GroupLayout.Alignment.LEADING);
+		
+		GroupLayout.SequentialGroup sGroup = gl_panelScrollContainer.createSequentialGroup();
+		GroupLayout.SequentialGroup s2Group = gl_panelScrollContainer.createSequentialGroup();
+		
+		GroupLayout.ParallelGroup pGroup = gl_panelScrollContainer.createParallelGroup(GroupLayout.Alignment.LEADING, false);
+		
+		
+		for(int i=0; i<availablePieces.length; i++){
+			pGroup.addComponent(availablePieces[i], GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE);
+			s2Group.addComponent(availablePieces[i], GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE);
+		}
+		
+		gl_panelScrollContainer.setHorizontalGroup(hGroup.addGroup(sGroup.addGroup(pGroup)));
+		gl_panelScrollContainer.setVerticalGroup(vGroup.addGroup(s2Group));
+		panelScrollContainer.setLayout(gl_panelScrollContainer);	
 	}
 		
 }
