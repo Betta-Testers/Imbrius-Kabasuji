2d9d4e28f674d7e0b25234fd2009b518cf7d5286
bwoconnor <bwoconnor@wpi.edu>
2016-04-03 18:15:24 -0400

    AvailableLevelView now up

--- src/gameMockups/SelectLevel.java
+++ src/gameMockups/SelectLevel.java
@@ -70,33 +70,8 @@ public class SelectLevel extends JFrame {
 		scrollPane.setViewportView(scrollablePanel);
 		
 		//level 1 select//
-		JPanel lvl1Panel = new JPanel();
+		AvailableLevelView lvl1Panel = new AvailableLevelView();
 		scrollablePanel.add(lvl1Panel);
-		
-		JToggleButton lvl1Toggle = new JToggleButton("Level 1");
-		
-		JLabel lvl1Stars = new JLabel("");
-		lvl1Stars.setIcon(new ImageIcon("C:\\Users\\Brendan\\Desktop\\Star image2.png"));
-		GroupLayout gl_lvl1Panel = new GroupLayout(lvl1Panel);
-		gl_lvl1Panel.setHorizontalGroup(
-			gl_lvl1Panel.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_lvl1Panel.createSequentialGroup()
-					.addGap(15)
-					.addGroup(gl_lvl1Panel.createParallelGroup(Alignment.LEADING)
-						.addComponent(lvl1Stars)
-						.addComponent(lvl1Toggle))
-					.addContainerGap(15, Short.MAX_VALUE))
-		);
-		gl_lvl1Panel.setVerticalGroup(
-			gl_lvl1Panel.createParallelGroup(Alignment.LEADING)
-				.addGroup(Alignment.TRAILING, gl_lvl1Panel.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(lvl1Toggle)
-					.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-					.addComponent(lvl1Stars)
-					.addContainerGap())
-		);
-		lvl1Panel.setLayout(gl_lvl1Panel);
 		
 		//level 2 select//
 		JPanel lvl2Panel = new JPanel();
