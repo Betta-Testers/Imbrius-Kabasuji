55bd97989c7a74e95280e6ccdaf9bbe653f9b092
ejbosia <ejbosia@wpi.edu>
2016-04-04 14:31:55 -0500

    Tweaked Exit Screen
    Removed replay and next level button. Changed "Quit" button to "Return
    to Level Select" and had it centered. Resized window

--- src/gameMockups/GameExitScreen.java
+++ src/gameMockups/GameExitScreen.java
@@ -43,14 +43,8 @@ public class GameExitScreen {
 	private void initialize() {
 		frmGameOver = new JFrame();
 		frmGameOver.setTitle("GAME OVER");
-		frmGameOver.setBounds(100, 100, 286, 161);
+		frmGameOver.setBounds(100, 100, 260, 160);
 		frmGameOver.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		JButton btnReplay = new JButton("Replay");
-		
-		JButton btnQuit = new JButton("Quit");
-		
-		JButton btnNewButton = new JButton("Next");
 		
 		JLabel lblNewLabel = new JLabel("");
 		lblNewLabel.setIcon(new ImageIcon(GameExitScreen.class.getResource("/gameMockups/star.png")));
@@ -60,43 +54,36 @@ public class GameExitScreen {
 		
 		JLabel lblNewLabel_2 = new JLabel("");
 		lblNewLabel_2.setIcon(new ImageIcon(GameExitScreen.class.getResource("/gameMockups/outline_star.png")));
+		
+		JButton btnReturnToLevel = new JButton("Return to Level Select");
 		GroupLayout groupLayout = new GroupLayout(frmGameOver.getContentPane());
 		groupLayout.setHorizontalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addContainerGap()
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addComponent(lblNewLabel)
-						.addComponent(btnNewButton))
 					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
 						.addGroup(groupLayout.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(lblNewLabel)
 							.addGap(37)
-							.addComponent(lblNewLabel_1))
+							.addComponent(lblNewLabel_1)
+							.addPreferredGap(ComponentPlacement.RELATED, 53, Short.MAX_VALUE)
+							.addComponent(lblNewLabel_2))
 						.addGroup(groupLayout.createSequentialGroup()
-							.addGap(29)
-							.addComponent(btnReplay)))
-					.addGap(31)
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addComponent(btnQuit)
-						.addComponent(lblNewLabel_2))
-					.addGap(30))
+							.addGap(43)
+							.addComponent(btnReturnToLevel)))
+					.addContainerGap())
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
 					.addGap(10)
 					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addGroup(groupLayout.createSequentialGroup()
-							.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-								.addComponent(lblNewLabel)
-								.addComponent(lblNewLabel_2))
-							.addGap(20)
-							.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-								.addComponent(btnNewButton)
-								.addComponent(btnQuit)
-								.addComponent(btnReplay)))
+						.addComponent(lblNewLabel)
+						.addComponent(lblNewLabel_2)
 						.addComponent(lblNewLabel_1))
-					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+					.addPreferredGap(ComponentPlacement.RELATED, 17, Short.MAX_VALUE)
+					.addComponent(btnReturnToLevel)
+					.addContainerGap())
 		);
 		frmGameOver.getContentPane().setLayout(groupLayout);
 	}
