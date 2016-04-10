446b1dd703fb15748506d0f41794f950a625037d
dfontana <dmz.sfx@gmail.com>
2016-04-09 20:26:47 -0400

    Comments

--- src/controllers/ExistingLevelEditController.java
+++ src/controllers/ExistingLevelEditController.java
@@ -13,35 +13,19 @@ public class ExistingLevelEditController implements java.awt.event.ActionListene
 		this.b = b;
 	}
 
-	/*
-	 * Actions to take:
-	 * Determine Level type and number from the button pressed
-	 * Open the file.
-	 * Initialize the model.
-	 * Set the BV model
-	 * Set the BV to display the right information for that level type
-	 * Show the builderView
-	 * Hide the levelTypeSelect
-	 * 
-	 * It will need to take a Builder object in the constructor to get the BV model
+	/**
+	 * Reads the source button for the level to be read into file and launched
+	 * in the editor. Directs flow of information in the builder. When all is done
+	 * the level should be displayed in the editor and the type select hidden
+	 * from view.
 	 */
-
 	public void actionPerformed(ActionEvent ae) {
-		//1) Read find the level type and ID from the Source button
 		ExistingLevelView sourceButton = (ExistingLevelView) ae.getSource();
 		int levelID = sourceButton.getLevelNumber();
 		String levelType = b.lookupLevelType(levelID);
-
-		//2) Code for reading a level, DOES NOT belong here, this is a level thing.
-		//   Instead, pass the constructor the fileName and put this in level model
 		String fileName = levelID+"_"+levelType+".txt";
 		b.setModelLevelEditing(fileName);
-		
-
-
-
-		//do something
-
-		System.out.println("Button " + sourceButton.getLevelNumber());
+		b.setBuilderViewVisible(true);
+		b.setLevelTypeSelectViewVisible(false);
 	}
 }
