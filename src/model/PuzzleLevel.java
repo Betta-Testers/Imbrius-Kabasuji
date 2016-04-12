c3acf2f7afa78632593da5c9cda738036d7d675e
dfontana <dmz.sfx@gmail.com>
2016-04-12 14:40:51 -0400

    Methods implemented in puzzle except saving

--- src/model/PuzzleLevel.java
+++ src/model/PuzzleLevel.java
@@ -69,19 +69,71 @@ public class PuzzleLevel extends AbstractLevelModel{
 		return false;
 	}
 
+	/** A level is complete if the total number of stars earned is 3, meaning there are no more moves to be made, the player
+	 * has achieved the most they can.
+	 * 
+	 * OR
+	 * 
+	 * The player is out of moves. 
+	 */
 	@Override
 	boolean isComplete() {
-		// TODO Auto-generated method stub
+		if(starsEarned == 3 || (moveLimit-movesMade) == 0){
+			return true;
+		}
+		
 		return false;
 	}
 
+	/**updateProgress occurs after every move is made. This updates the stars earned for the current level if
+	 * the number of tiles left uncovered on the board is remaining is 0/6/12. The starsEarned is set rather than incremented
+	 * to prevent duplicate triggers of the same threshold (Since you can moves a piece off a board and back onto it).
+	 * 
+	 * This also means it is possible to lower a player's current star count due to moving a piece off the board, reducing the
+	 * number of tiles covered.
+	 * 
+	 * After all checks are made, the level is saved if the current play through has earned more stars than 
+	 * the number tracked on file.
+	 */
 	@Override
 	void updateProgress() {
-		// TODO Auto-generated method stub
 		
+		switch(tilesLeft){
+			case(12):
+				starsEarned = 1;
+				break;
+			case(6):
+				starsEarned = 2;
+				break;
+			case(0):
+				starsEarned = 3;
+				break;
+			default:
+				starsEarned = 0;
+		}
+		
+		//TODO This line is now in the wrong place. If a player drops their total stars earned during play, but exceeded their best at one point, this would be an
+		//in accurate reflection of their actual performance
+		if(starsEarned > maxStarsEarned){
+			saveProgressInFile();
+		}
 	}
 	
+	/**
+	 * AdjustTileCount takes in a detla and offsets the tilesLeft to be converted on the board
+	 * by that amount. The ultimate goal is to get the tilesLeft value to 0.
+	 * @param delta A positive or negative integer representing the number of tiles a piece is replacing the board
+	 * This value is *anticipated* to always be +- 6
+	 */
 	public void adjustTileCount(int delta){
-		
+		tilesLeft += delta;
+	}
+	
+	/**
+	 * incrementMovesMade is called whenever a move is performed in a puzzle level. This includes bullpen to board,
+	 * board to board, or off the board. It always increments by 1.
+	 */
+	public void incrementMovesMade(){
+		movesMade++;
 	}
 }
