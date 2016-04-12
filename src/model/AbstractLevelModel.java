ad93478c80446c84b6352272680c5097e9c43ef8
dfontana <dmz.sfx@gmail.com>
2016-04-11 22:34:37 -0400

    Outlined Abstract, initialized subclasses

--- src/model/AbstractLevelModel.java
+++ src/model/AbstractLevelModel.java
@@ -28,18 +28,29 @@ public abstract class AbstractLevelModel {
 	
 	/**sourceFile is the file on disk associated with THIS level. If execution has reached this phase, this file
 	 * ALWAYS exists - builder or player. Why? Because the Game class is in charge of loading the levels at launch
-	 * and the Builder class is will create a placeholder file for the level if it's creating a new level!**/
+	 * and the Builder class is will create a placeholder file for the level if it's creating a new level!
+	 * TODO Verify if this is the 
+	 **/
 	File sourceFile;
 
 	/**You CANNOT instantiate an AbstractLevelModel. This constructor is here so you can super() set the 
 	 * final fields within the subclasses. Otherwise, these fields would have a hard time being set AND
 	 * being final!
-	 * **/
+	 **/
 	AbstractLevelModel(int levelID, String levelType, boolean canMovePiece){
 		this.levelID = levelID;
 		this.levelType = levelType;
 		this.canMovePiece = canMovePiece;
 	}
+	
+	abstract boolean isComplete();
+	
+	//Player Save Method - Boolean is to tell if file could not be opened
+	abstract boolean saveProgressInFile();
+	
+	//Builder Save Method - Boolean is to tell if file could not be opened
+	//When editing, overwrite old file.
+	abstract boolean saveLevelToFile();
 	
 	/**
 	 * Method capable of opening the given file and reading it line by
