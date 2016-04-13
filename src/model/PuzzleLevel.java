86acb41155ba86c3a3e9e3d22ffa42322f67e09e
Hans Johnson <hejohnson@wpi.edu>
2016-04-13 00:57:37 -0400

    Created stub with method for controller to access

---
+++ src/model/PuzzleLevel.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package model;
+
+/**
+ * @author hejohnson
+ *
+ */
+public class PuzzleLevel extends AbstractLevelModel {
+
+	/**
+	 * 
+	 */
+	public PuzzleLevel() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void setNumberMoves(int numberMoves) {
+		System.out.println(numberMoves);
+	}
+
+}
