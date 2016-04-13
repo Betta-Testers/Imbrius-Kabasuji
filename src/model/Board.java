a75580c049e3427e8c8ed4bd2cba73b215df4282
bwoconnor <bwoconnor@wpi.edu>
2016-04-13 13:39:41 -0400

    Board TODOs added
    Clarification pending for some methods and their params/returns

--- src/model/Board.java
+++ src/model/Board.java
@@ -1,5 +1,36 @@
 package model;
 
 public class Board {
-
+	AbstractTile board[][] = new AbstractTile[12][12];
+	
+	int tileSize = 32;
+	
+	public Board(){
+		
+	}
+	
+//	boolean putPieceOnBoard(Piece p){
+		//TODO once piece is solidified, figure out how to properly add it to the matrix
+//	}
+	
+	void resetBoard(){
+		//TODO: Reset board so that no pieces are on it, piece tiles will have a list of what they replaced. 
+		//needs to return something that will contain 
+	}
+	
+	//boolean deletePiece(Piece p){
+		//TODO: Will need to remove piece from board, and delete it from the list of pieces on the board
+		// Should this still just return a boolean? or should it return the piece that was removed?
+	//}
+	
+	//AbstractTile swapTile(AbstractTile bt){
+		
+	//}
+	
+	AbstractTile getTileAt(int x, int y){
+		int row = x/tileSize;
+		int column = y/tileSize;
+		
+		return board[row][column];
+	}
 }
