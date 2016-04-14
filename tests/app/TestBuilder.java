d8b1855caf07653576e7d9098b04446d41dd8572
dfontana <dmz.sfx@gmail.com>
2016-04-13 21:24:13 -0400

    Stubbed testCases

---
+++ tests/app/TestBuilder.java
@@ -0,0 +1,32 @@
+package app;
+
+import junit.framework.TestCase;
+
+public class TestBuilder extends TestCase {
+
+	@Override
+	protected void setUp(){
+		
+	}
+	
+	@Override
+	protected void tearDown(){
+		
+	}
+	
+	public void testSaveStarMap(){
+		
+	}
+	
+	public void testLoadStarMap(){
+		
+	}
+	
+	public void testSaveLevel(){
+		
+	}
+	
+	public void testLoadLevel(){
+		
+	}
+}
