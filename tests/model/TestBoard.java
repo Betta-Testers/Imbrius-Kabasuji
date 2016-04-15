e434266c709807509960cdcdacf63ae331a1d622
bwoconnor <bwoconnor@wpi.edu>
2016-04-15 00:12:25 -0400

    Test created, had to stop because of piece issues.

--- tests/model/TestBoard.java
+++ tests/model/TestBoard.java
@@ -13,6 +13,15 @@ import java.awt.Color;
 
 public class TestBoard extends TestCase{
 	public void testBoard(){
+		Board b = new Board();
+		b.board[0][0] = new BoardTile(0,0);
+		b.board[1][0] = new BoardTile(1,0);
+		b.board[2][0] = new BoardTile(2,0);
+		b.board[3][0] = new BoardTile(3,0);
+		b.board[4][0] = new BoardTile(4,0);
+		b.board[5][0] = new BoardTile(5,0);
+
+		
 		
 	}
 
