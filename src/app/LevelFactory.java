00732d0c66d290744562103df7aa055c471b06fa
dfontana <dmz.sfx@gmail.com>
2016-04-16 12:31:05 -0400

    Working on more functionality to LevelFactory. More to come.

--- src/app/LevelFactory.java
+++ src/app/LevelFactory.java
@@ -17,68 +17,117 @@ import model.ReleaseLevel;
  *
  */
 public class LevelFactory{
-	final String directory;
+	String directory;
 	StarMap levels;
 
 	public static void main(String[] args){
-		LevelFactory factory = new LevelFactory("./imbriusLevelFiles/");
+		LevelFactory factory = new LevelFactory();
+		factory.quick15("./imbriusLevelFiles/");
 	}
 	
-	public LevelFactory(String directory){
+	public LevelFactory(){
 		System.out.println("Factory Started...");
+	}
+	
+	/**
+	 * Generates 15 levels, 5 of each type going Puzzle, Release, Lightning, Puzzle..
+	 * Saves each to disk and stores each one into the StarMap.
+	 * Good quick testing needs
+	 * @param directory -> location where the files will be made
+	 */
+	public void quick15(String directory){
 		this.directory = directory;
 		this.levels = new StarMap(directory);
-		
 		for(int i=1; i <= 5; i++){
-			saveLevel(GeneratePuzzle());
-			saveLevel(GenerateLightning());
-			saveLevel(GenerateRelease());
+			PuzzleLevel pl = GeneratePuzzle(levels.nextOpenID(), 15);
+			saveLevel(pl);
+			addToData(pl, 2);
+			LightningLevel ll = GenerateLightning(levels.nextOpenID(), 15);
+			saveLevel(ll);
+			addToData(ll, 3);
+			ReleaseLevel rl = GenerateRelease(levels.nextOpenID(), 15);
+			saveLevel(rl);
+			addToData(rl, 1);
 		}
 	}
 	
-	
-	
-	ReleaseLevel GenerateRelease() {
-		int id = levels.nextOpenID();
+	/**
+	 * Generates a ReleaseLevel with the given ID and bullpen size
+	 * Note the bullpen is random in this case. If you want to specify
+	 * the exact pieces in the bullpen/board, use the alternate method
+	 * @param id	- LevelID
+	 * @param bullpenSize - Size of randomly generated bullpen
+	 * @return ReleaseLevel
+	 */
+	ReleaseLevel GenerateRelease(int id, int bullpenSize) {
 		System.out.println("	Generating Release @"+id);
 		ReleaseLevel rl = new ReleaseLevel(id);
 		Board board = new Board();
-		Bullpen bullpen = new Bullpen(id);
+		Bullpen bullpen = new Bullpen(bullpenSize);
 		rl.setBoard(board);
 		rl.setBullpen(bullpen);
 		return rl;
 	}
 
-
-
-	LightningLevel GenerateLightning() {
-		int id = levels.nextOpenID();
+	/**
+	 * Generates a LightningLevel with the given ID and bullpen size
+	 * Note the bullpen is random in this case. If you want to specify
+	 * the exact pieces in the bullpen/board, use the alternate method
+	 * @param id	- LevelID
+	 * @param bullpenSize - Size of randomly generated bullpen
+	 * @return LightningLevel
+	 */
+	LightningLevel GenerateLightning(int id, int bullpenSize) {
 		System.out.println("	Generating Lightning @"+id);
 		LightningLevel ll = new LightningLevel(id);
 		Board board = new Board();
-		Bullpen bullpen = new Bullpen(id);
+		Bullpen bullpen = new Bullpen(bullpenSize);
 		ll.setBoard(board);
 		ll.setBullpen(bullpen);
 		ll.setTotalTime(200);
 		return ll;
 	}
 
-
-
-	PuzzleLevel GeneratePuzzle() {
-		int id = levels.nextOpenID();
+	/**
+	 * Generates a puzzle level with the given ID and bullpen size
+	 * Note the bullpen is random in this case. If you want to specify
+	 * the exact pieces in the bullpen/board, use the alternate method
+	 * @param id	- LevelID
+	 * @param bullpenSize - Size of randomly generated bullpen
+	 * @return PuzzleLevel
+	 */
+	PuzzleLevel GeneratePuzzle(int id, int bullpenSize) {
 		System.out.println("	Generating Puzzle @"+id);
 		PuzzleLevel pl = new PuzzleLevel(id);
 		Board board = new Board();
-		Bullpen bullpen = new Bullpen(id);
+		Bullpen bullpen = new Bullpen(bullpenSize);
 		pl.setBoard(board);
 		pl.setBullpen(bullpen);
 		pl.setMoveLimit(20);
 		return pl;
 	}
-
-
-
+	
+	/**
+	 * Adds a given model and the stars assigned to it into the StarMap. This will cause the
+	 * StarMap to save itself. Can only add a model whose ID is not already in the starMap of 
+	 * this Factory.
+	 * @param m - AbstractLevelModel (Puzzle, Release, Lightning)
+	 * @param starsEarned - Max Stars you wish to assign to it
+	 */
+	public void addToData(AbstractLevelModel m, int starsEarned){
+		String type = m.getType();
+		int id = m.getID();
+		System.out.println("		LastKey:"+levels.lastID()+"Current:"+id);
+		if(!levels.containsKey(id)){
+			levels.put(id, type);
+			levels.setMaxStars(id, starsEarned);
+		}
+	}
+	
+	/**
+	 * Saves the given level to the current directory assigned in this factory
+	 * @param level - Level you wish to save to disk
+	 */
 	public void saveLevel(AbstractLevelModel level){
 		ObjectOutputStream oos = null;
 
@@ -96,11 +145,17 @@ public class LevelFactory{
 		if (oos != null) {
 			try { oos.close(); } catch (IOException ioe) { } 
 		}
+	}
 
-		System.out.println("		LastKey:"+levels.lastID()+"Current:"+id);
-		if(id > levels.lastID()){
-			levels.put(id, type);
-			levels.setMaxStars(id, 1);
-		}
+	/**
+	 * Set Directory changes the directory this factory points to. Doing this forces a 
+	 * recreation of the StarMap in this factory, which will subsequently change the
+	 * one of DISK. Use this method carefully. Useful for testing, when you want to 
+	 * clear a directory and rebase the StarMap.
+	 * @param directory - Location on disk of where you want to point the factory to
+	 */
+	public void setDirectory(String directory){
+		this.directory = directory;
+		this.levels = new StarMap(directory);
 	}
 }
