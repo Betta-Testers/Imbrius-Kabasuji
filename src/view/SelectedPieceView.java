9fc377ad02c1dc9d0637075098097c3a09af2acd
dfontana <dmz.sfx@gmail.com>
2016-04-17 22:00:53 -0400

    Cleaned comment

--- src/view/SelectedPieceView.java
+++ src/view/SelectedPieceView.java
@@ -10,9 +10,6 @@ import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.JLabel;
 
 public class SelectedPieceView extends JPanel{
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	GroupLayout groupLayout;
 	JButton btnRotateLeft;
