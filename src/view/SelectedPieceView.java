fb0a27a412facb3b90b2760a1b60ec44ed5bad40
dfontana <dmz.sfx@gmail.com>
2016-04-20 12:46:10 -0400

    SelectedPieceView ready

--- src/view/SelectedPieceView.java
+++ src/view/SelectedPieceView.java
@@ -1,6 +1,10 @@
 package view;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+
 import javax.swing.JPanel;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
@@ -8,7 +12,9 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.LayoutStyle.ComponentPlacement;
 
+import model.AbstractTile;
 import model.Bullpen;
+import model.PieceTile;
 
 public class SelectedPieceView extends JPanel{
 	private static final long serialVersionUID = 1L;
@@ -19,6 +25,10 @@ public class SelectedPieceView extends JPanel{
 	JButton btnFlipX;
 	JButton btnFlipY;
 	Bullpen bp;
+	
+	/** Double Buffering technique requires an offscreen image. */
+	Image offscreenImage;
+	Graphics offscreenGraphics;
 	
 	public SelectedPieceView(Bullpen bp){
 		setPreferredSize(new Dimension(384, 224));
@@ -36,6 +46,73 @@ public class SelectedPieceView extends JPanel{
 		setupLayout();
 	}
 
+	/**
+	 * Redraw recreates everything offscreen. Notice how there is an offscreen image and graphics.
+	 * It even paints all shapes to the offscreen graphics supplied to the paintShape object
+	 * 		-> It is believed that the offscreenGraphics is an object stored within the offscreenImage
+	 * 		-> This is the method called BEFORE calling .paint() on this panel
+	 */
+	public void redraw() {
+		// nothing to draw into? Must stop here.
+		if (offscreenImage == null) return;
+		if (offscreenGraphics == null) return;    // detected during testing
+		
+		// clear the image.
+		offscreenGraphics.clearRect(0, 0, this.getWidth(), this.getHeight());
+		
+		/**
+		 * X and Y center of the jPanel, offset by half a tile width to make the tile
+		 * appear in the center, not the top left corner
+		 */
+		int xCenter = 96-16;
+		int yCenter = 96-16;
+		
+		for(PieceTile t: bp.getSelectedPiece().getTiles()){
+			int xCoord = (t.getCol()*32)+xCenter;
+			int yCoord = (t.getRow()*32)+yCenter;
+			
+			offscreenGraphics.setColor(Color.BLACK);
+			offscreenGraphics.drawRect(xCoord, yCoord, 32, 32);
+			offscreenGraphics.setColor(t.getColor());
+			offscreenGraphics.fillRect(xCoord, yCoord, 32, 32);	
+		}	
+	}
+	
+	/**
+	 * Ensure image available prepares the offscreen image for painting if it is currently missing from
+	 * the object (null). It gets called only INSIDE this class and is called in the paintComponent()
+	 * method
+	 */
+	void ensureImageAvailable(Graphics g) {
+		if (offscreenImage == null) {  
+			offscreenImage = this.createImage(this.getWidth(), this.getHeight());
+			offscreenGraphics = offscreenImage.getGraphics();			
+	
+			redraw();
+		}
+	}
+	
+	/** 
+	 * This is the method called by .paint(). It supers the constructor and then called ensureImageAvailable
+	 * to make sure there is an offscreen image to bring onto the board. THAT'S why redraw() is called before
+	 * paint(), so that way the offscreen is ready to go before it is brought onscreen.
+	 * 
+	 * After that, the paint method draws the image as it is, to this panel with g.drawImage.
+	 * It then draws every one of his shapes over that image.
+	 * 
+	 * To Draw within a JPanel, you need to have a protected void method of this name.
+	 * Note that the first operation of this method MUST BE to invoke super.paintComponent(g)
+	 * 
+	 * @param g
+	 */
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		ensureImageAvailable(g);
+		g.drawImage(offscreenImage, 0, 0, getWidth(), getHeight(), this);
+	}
+	
 	private void setupLayout(){
 		
 		btnRotateLeft.setIcon(new ImageIcon(SelectedPieceView.class.getResource("/icons/RotateLeft.png")));
--- src/view/TileView.java
+++
@@ -1,8 +0,0 @@
-package view;
-
-import model.AbstractTile;
-import java.awt.Graphics;
-
-public class TileView {
-  public void drawToBoard(Graphics g, AbstractTile t){}
-}
