6867854efb5419479ce9490207aa5f68a89ca455
dfontana <dmz.sfx@gmail.com>
2016-04-23 15:27:07 -0400

    Making controller into move

--- src/controllers/builder/BullpenPieceGroupSpinnerController.java
+++ src/controllers/builder/BullpenPieceGroupSpinnerController.java
@@ -16,20 +16,30 @@ public class BullpenPieceGroupSpinnerController implements ChangeListener {
 	BuilderPieceGroupView app;
 	PieceGroup model;
 	
+	/** Prevents multiple pushes to the undo stack when the move for this controller is undone **/
+	boolean doUpdate;
+	
 	public BullpenPieceGroupSpinnerController(BuilderPieceGroupView app, PieceGroup model) {
 		this.app = app;
 		this.model = model;
+		this.doUpdate = true;
 	}
 
-	@Override
 	/**
-	 * Handles increments and decrements to the PieceGroup
-	 * 
-	 * @param e ChangeEvent
+	 * When the spinner is changed, the controller detects it and logs the 
+	 * change as a move so it can be undone/redone.
 	 */
+	@Override
 	public void stateChanged(ChangeEvent e) {
-		int val = (int) app.getSpinner().getValue();
-		model.setCount(val);
+		if(doUpdate){
+			doUpdate = false;
+			
+			//DO STUFF
+			int val = (int) app.getSpinner().getValue();
+			model.setCount(val);
+			
+			doUpdate = true;
+		}
 	}
 	
 	
