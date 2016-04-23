0023f99cdf2cf2fa437770588420e6c83e828e29
Abby Harrison <awharrison@ABBYSMACPRO.WIFI.WPI.EDU>
2016-04-22 21:11:25 -0400

    TestBuilderPuzzleLightning throwing NullPointerException at willFit check

--- tests/controllers/TestBuilderPuzzleLightning.java
+++ tests/controllers/TestBuilderPuzzleLightning.java
@@ -1,12 +1,17 @@
 package controllers;
 
 import java.io.File;
+import java.util.ArrayList;
+
 import controllers.builder.SwapTileBoardToEmptyMove;
 import controllers.builder.SwapTileEmptyToBoardMove;
 import controllers.common.Move;
+import controllers.common.PlacePieceOnBoardFromBullpenMove;
 import model.Board;
 import model.BoardTile;
+import model.Bullpen;
 import model.EmptyTile;
+import model.PieceGroup;
 import model.PuzzleLevel;
 import view.BoardView;
 import view.BuilderView;
@@ -24,8 +29,9 @@ public class TestBuilderPuzzleLightning extends TestCase {
 	Builder build;
 	BuilderView buildView;
 	PuzzleLevel lvl;
-	Board releaseBoard;
+	Board puzzleBoard;
 	BoardView boardView;
+	Bullpen bullpen;
 	BullpenView bpView;
 	
 	@Override
@@ -35,9 +41,8 @@ public class TestBuilderPuzzleLightning extends TestCase {
 		build.createPuzzleLevel();
 		buildView = build.getBuilderView();
 		lvl = (PuzzleLevel)build.getCurrentLevel();
-		releaseBoard = lvl.getBoard();
+		puzzleBoard = lvl.getBoard();
 		boardView = buildView.getBoardView();
-		bpView = buildView.getBullpenView();
 	}
 	
 	public void testPuzzleLightningBoard() {
@@ -47,27 +52,60 @@ public class TestBuilderPuzzleLightning extends TestCase {
 	//====================== Empty to Board Tile to Empty Swapping ======================//
 	public void testTileEmptyToBoardSwapMove() {
 		Move m;
-		m = new SwapTileEmptyToBoardMove(buildView, (EmptyTile)releaseBoard.getTileAt(boardView.getX(), boardView.getX()), lvl);
+		m = new SwapTileEmptyToBoardMove(buildView, (EmptyTile)puzzleBoard.getTileAt(boardView.getX(), boardView.getX()), lvl);
 		assertTrue(m.doMove());
-		assertEquals(1, releaseBoard.getNumBoardTiles());
+		assertEquals(1, puzzleBoard.getNumBoardTiles());
 		m.undo();
-		assertEquals(0, releaseBoard.getNumBoardTiles());
+		assertEquals(0, puzzleBoard.getNumBoardTiles());
 		m.redo();
-		assertEquals(1, releaseBoard.getNumBoardTiles());
+		assertEquals(1, puzzleBoard.getNumBoardTiles());
 		
-		m = new SwapTileBoardToEmptyMove(buildView, (BoardTile)releaseBoard.getTileAt(boardView.getX(), boardView.getX()), lvl);
+		m = new SwapTileBoardToEmptyMove(buildView, (BoardTile)puzzleBoard.getTileAt(boardView.getX(), boardView.getX()), lvl);
 		assertTrue(m.doMove());
-		assertEquals(0, releaseBoard.getNumBoardTiles());
+		assertEquals(0, puzzleBoard.getNumBoardTiles());
 		m.undo();
-		assertEquals(1, releaseBoard.getNumBoardTiles());
+		assertEquals(1, puzzleBoard.getNumBoardTiles());
 		m.redo();
-		assertEquals(0, releaseBoard.getNumBoardTiles());
+		assertEquals(0, puzzleBoard.getNumBoardTiles());
 	}
 	
 	
 	//====================== Place Piece on Board from Bullpen Move ======================//
 	public void testMovePiecesOnBoardFromBullpen() {
 		Move m;
+		
+		/*
+		 * initialize bullpen
+		 */
+		ArrayList<PieceGroup> pieces = new ArrayList<PieceGroup>();
+		pieces.add(new PieceGroup(1,4));
+		lvl.setBullpen(new Bullpen(pieces));
+		
+		/*
+		 * swap tiles to be able to place pieces
+		 */
+		puzzleBoard.swapTile(new BoardTile(0, 0));
+		puzzleBoard.swapTile(new BoardTile(1, 0));
+		puzzleBoard.swapTile(new BoardTile(2, 0));
+		assertEquals(3, puzzleBoard.getNumBoardTiles());
+		/*
+		 * try to place piece (test willfit)
+		 */
+		m = new PlacePieceOnBoardFromBullpenMove(lvl, puzzleBoard.getTileAt(2*puzzleBoard.getTileSize(), 0), bpView);
+		
+		// TODO figure out why willFit is returning a nullPointerException
+		puzzleBoard.willFit(lvl.getBullpen().getSelectedPiece(), 2, 0);
+		assertFalse(m.doMove());
+		
+		/*
+		 * complete space for piece
+		 */
+		puzzleBoard.swapTile(new BoardTile(3, 0));
+		puzzleBoard.swapTile(new BoardTile(4, 0));
+		puzzleBoard.swapTile(new BoardTile(5, 0));
+		assertTrue(m.doMove());
+		assertEquals(6, puzzleBoard.getNumBoardTiles());
+		
 	}
 	
 	//====================== Move Piece Board to Board Move ======================//
